a2a07186426eb667bf360ce73f7396a8d952540a
Marisen420 <christina.c.cc26@gmail.com>
2023-10-12 09:06:12 -0400

    first commit

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <main className="About">
+      <h2>About</h2>
+      <p style={{ margintop: "1rem"}}>Hi, I am Christina.I'm a frontend developer with a passion for creating engaging and user-friendly web experiences</p>
+    </main>
+  )
+}
+
+export default About
